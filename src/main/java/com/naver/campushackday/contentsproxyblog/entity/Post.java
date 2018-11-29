a0dda33a86c9bb625ffe5cbfd683b81262f1c488
so3500 <dltjddhs@gmail.com>
2018-11-30 01:33:33 +0900

    chore: 미사용 import 제거 (#25)

--- src/main/java/com/naver/campushackday/contentsproxyblog/entity/Post.java
+++ src/main/java/com/naver/campushackday/contentsproxyblog/entity/Post.java
@@ -5,8 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
+import javax.persistence.*;g
 
 @Getter
 @Setter
