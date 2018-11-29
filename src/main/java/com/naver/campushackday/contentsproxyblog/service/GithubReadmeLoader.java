801d35971d915f60bf18c143ae76081e2becd996
Jiwon Bae <jwb8705@gmail.com>
2018-11-29 20:05:19 +0900

    feat: GithubReadmeLoader 구성, 테스트 코드 작성

--- src/main/java/com/naver/campushackday/contentsproxyblog/service/GithubReadmeLoader.java
+++ src/main/java/com/naver/campushackday/contentsproxyblog/service/GithubReadmeLoader.java
@@ -1,11 +1,29 @@
 package com.naver.campushackday.contentsproxyblog.service;
 
-import com.sun.corba.se.impl.util.RepositoryId;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.egit.github.core.RepositoryContents;
+import org.eclipse.egit.github.core.RepositoryId;
+import org.eclipse.egit.github.core.service.ContentsService;
+import org.eclipse.egit.github.core.util.EncodingUtils;
 import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
 
 @Component
 @Slf4j
 public class GithubReadmeLoader {
+
+    public String fetchMarkdownFileAndConvertToString(String repositoryURL, String filePath) throws Exception {
+        return convertMarkdownByteArrayToString(fetchGithubMarkdownFile(repositoryURL, filePath));
+    }
+
+    private RepositoryContents fetchGithubMarkdownFile(String repositoryURL, String filePath) throws Exception {
+        String path = filePath.replace(repositoryURL, "");
+        return new ContentsService().getContents(RepositoryId.createFromUrl(repositoryURL), path).get(0);
+    }
+
+    private String convertMarkdownByteArrayToString(RepositoryContents mdFile) {
+        return new String(EncodingUtils.fromBase64(mdFile.getContent()), StandardCharsets.UTF_8);
+    }
 
 }
