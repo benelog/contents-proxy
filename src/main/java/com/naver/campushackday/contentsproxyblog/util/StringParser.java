e42acc66db6e797928da6c8d938cf4f787b36eba
hyukjin-lee <gogokow27@gmail.com>
2018-11-30 01:51:54 +0900

    feat : 상대경로를 추출하기 위한 regex 추가 (#26)

--- src/main/java/com/naver/campushackday/contentsproxyblog/util/StringParser.java
+++ src/main/java/com/naver/campushackday/contentsproxyblog/util/StringParser.java
@@ -1,8 +1,28 @@
 package com.naver.campushackday.contentsproxyblog.util;
 
-public class StringParser {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-	public static String[] parseGithubUrl(String url){
-		return url.split("/blob/master");
+public class StringParser {
+	private static final String URL_SPLITOR = "/blob/master";
+	private static final String IMAGE_URL_REGEX = "(?:!\\[(.*?)\\]\\((.*?)\\))";
+
+	public static String[] parseGithubUrl(String url) {
+		return url.split(URL_SPLITOR);
+	}
+
+	public static List<String> parseMarkdownImageUrl(String markdownText) {
+		List<String> allMatches = new ArrayList<>();
+		Matcher m = Pattern.compile(IMAGE_URL_REGEX).matcher(markdownText);
+
+		while (m.find()) {
+			String imageTag = m.group();
+			String imageUrl = imageTag.substring(imageTag.indexOf("(") + 1, imageTag.length() - 1);
+			allMatches.add(imageUrl);
+		}
+
+		return allMatches;
 	}
 }
