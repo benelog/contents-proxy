2e129bec8157bca3e91d902a92364c4885e52b3b
hyukjin-lee <gogokow27@gmail.com>
2018-11-29 17:36:37 +0900

    fix : 렌더링 함수 리팩토링

--- src/main/java/com/naver/campushackday/contentsproxyblog/component/Markdown.java
+++ src/main/java/com/naver/campushackday/contentsproxyblog/component/Markdown.java
@@ -4,20 +4,25 @@ import com.vladsch.flexmark.ast.Node;
 import com.vladsch.flexmark.html.HtmlRenderer;
 import com.vladsch.flexmark.parser.Parser;
 import com.vladsch.flexmark.util.options.MutableDataSet;
+import org.springframework.orm.jpa.persistenceunit.MutablePersistenceUnitInfo;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Markdown {
 
-    public String renderMarkdownTextToHtml (String markdownText){
-        MutableDataSet options = new MutableDataSet();
+    private MutableDataSet options;
+    private Parser parser;
 
-        Parser parser = Parser.builder(options).build();
-        HtmlRenderer renderer = HtmlRenderer.builder(options).build();
-
-        Node document = parser.parse("This is *Sparta*");
-        String html = renderer.render(document);  // "<p>This is <em>Sparta</em></p>\n"
-        return html;
+    public Markdown() {
+        options = new MutableDataSet();
+        parser = Parser.builder(options).build();
     }
 
+    public String renderMarkdownTextToHtml(String markdownText) {
+        HtmlRenderer renderer = HtmlRenderer.builder(options).build();
+
+        Node document = parser.parse(markdownText);
+        String html = renderer.render(document);  // "<p>This is <em>HackDay</em></p>\n"
+        return html;
+    }
 }
