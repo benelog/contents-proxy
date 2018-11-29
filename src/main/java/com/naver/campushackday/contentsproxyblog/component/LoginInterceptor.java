6377220e2635db97c92b1c732c5b1d6f5742a568
Jiwon Bae <jwb8705@gmail.com>
2018-11-30 07:51:38 +0900

    feat: Login 처리를 위한 인터셉터 생성 (#27)

---
+++ src/main/java/com/naver/campushackday/contentsproxyblog/component/LoginInterceptor.java
@@ -0,0 +1,16 @@
+package com.naver.campushackday.contentsproxyblog.component;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return false;
+    }
+}
