a7846bd0f72d2b51665a895b01099963e2d6c87d
Jiwon Bae <jwb8705@gmail.com>
2018-11-30 08:23:36 +0900

    feat: 관리자 로그인 기능 구현 (#27)

---
+++ src/main/java/com/naver/campushackday/contentsproxyblog/presentation/AdminController.java
@@ -0,0 +1,37 @@
+package com.naver.campushackday.contentsproxyblog.presentation;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private String adminId;
+    private String adminPassword;
+
+    public AdminController(@Value("${admin.id}") String adminId, @Value("${admin.password}") String adminPassword){
+        this.adminId = adminId;
+        this.adminPassword = adminPassword;
+    }
+
+    @GetMapping("/loginForm")
+    public String loginForm(){
+        return "/admin/login";
+    }
+
+    @PostMapping("/login")
+    public String login(String userId, String password) {
+        if(userId.equals(adminId) && password.equals(adminPassword)){
+            return "redirect:/post/form";
+        }
+        return "redirect:/";
+    }
+
+}
