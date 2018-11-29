2353ab84f96f75712407c64fc1b6d298f252ca94
hyukjin-lee <gogokow27@gmail.com>
2018-11-30 01:52:20 +0900

    feat : string parser 상대경로 메소드 테스트 추가 (#26)

--- src/test/java/com/naver/campushackday/contentsproxyblog/util/StringParserTest.java
+++ src/test/java/com/naver/campushackday/contentsproxyblog/util/StringParserTest.java
@@ -2,6 +2,8 @@ package com.naver.campushackday.contentsproxyblog.util;
 
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -12,5 +14,11 @@ public class StringParserTest {
 		String[] urlElements = StringParser.parseGithubUrl("https://github.com/naver/pinpoint/blob/master/.github/ISSUE_TEMPLATE/bug_report.md");
 		assertThat(urlElements[0], is("https://github.com/naver/pinpoint"));
 		assertThat(urlElements[1], is("/.github/ISSUE_TEMPLATE/bug_report.md"));
+	}
+
+	@Test
+	public void parseMarkdownImageUrl_TEST() {
+		List<String> imageUrls = StringParser.parseMarkdownImageUrl("![sisi](/fdafdaf/fdafd/fad) fdaf  ![](/fdaf/fda)");
+		assertThat(imageUrls.get(1), is("/fdaf/fda"));
 	}
 }
