8f29472849d15b44c8e933add745e4e7007dd9d7
so3500 <dltjddhs@gmail.com>
2018-11-29 22:55:34 +0900

    chore: StringParserTest 에서 사용하는 메소드 명 변경

--- src/test/java/com/naver/campushackday/contentsproxyblog/util/StringParserTest.java
+++ src/test/java/com/naver/campushackday/contentsproxyblog/util/StringParserTest.java
@@ -2,8 +2,6 @@ package com.naver.campushackday.contentsproxyblog.util;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -11,9 +9,8 @@ public class StringParserTest {
 
 	@Test
 	public void URL_PARSE_TEST() {
-		String[] urlElements = StringParser.parseUrl("https://github.com/naver/pinpoint/blob/master/.github/ISSUE_TEMPLATE/bug_report.md");
+		String[] urlElements = StringParser.parseGithubUrl("https://github.com/naver/pinpoint/blob/master/.github/ISSUE_TEMPLATE/bug_report.md");
 		assertThat(urlElements[0], is("https://github.com/naver/pinpoint"));
 		assertThat(urlElements[1], is("/.github/ISSUE_TEMPLATE/bug_report.md"));
-
 	}
 }
