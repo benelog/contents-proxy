3390e82030ac56d2a6994e8de0c61cc0cae8f107
hyukjin-lee <gogokow27@gmail.com>
2018-11-29 21:43:14 +0900

    feat : blogsetting Repository Title 찾기 구현 (#20)

--- src/main/java/com/naver/campushackday/contentsproxyblog/persistence/BlogSettingRepository.java
+++ src/main/java/com/naver/campushackday/contentsproxyblog/persistence/BlogSettingRepository.java
@@ -3,5 +3,6 @@ package com.naver.campushackday.contentsproxyblog.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.naver.campushackday.contentsproxyblog.entity.BlogSetting;
 
-public interface BlogSettingRepository extends JpaRepository<BlogSetting, Integer> {
+public interface BlogSettingRepository extends JpaRepository<BlogSetting, Long> {
+    String findTitleById(Long id);
 }
