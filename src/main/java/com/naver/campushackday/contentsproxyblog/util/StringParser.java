56ec0112560396504b8769c7b7c9598581bd176e
hyukjin-lee <gogokow27@gmail.com>
2018-11-30 11:48:26 +0900

    refactor : 하드코딩 최소화

--- src/main/java/com/naver/campushackday/contentsproxyblog/util/StringParser.java
+++ src/main/java/com/naver/campushackday/contentsproxyblog/util/StringParser.java
@@ -7,8 +7,8 @@ import java.util.regex.Pattern;
 
 public class StringParser {
 	private static final String URL_SPLITOR = "/blob/master";
-	private static final String IMAGE_URL_REGEX = "(?:!\\[(.*?)\\]\\((.*?)\\))";
-	private static final Pattern IMAGE_URL_PATTERN = Pattern.compile(IMAGE_URL_REGEX);
+	private static final String IMAGE_TAG_SPLITOR = "(";
+	private static final Pattern IMAGE_URL_PATTERN = Pattern.compile("(?:!\\[(.*?)\\]\\((.*?)\\))");
 
 	public static String[] parseGithubUrl(String url) {
 		return url.split(URL_SPLITOR);
@@ -16,14 +16,18 @@ public class StringParser {
 
 	public static List<String> parseMarkdownImageUrl(String markdownText) {
 		List<String> allMatches = new ArrayList<>();
+
+		matchRegex(markdownText, allMatches);
+		return allMatches;
+	}
+
+	private static void matchRegex(String markdownText, List<String> allMatches) {
 		Matcher m = IMAGE_URL_PATTERN.matcher(markdownText);
 
 		while (m.find()) {
 			String imageTag = m.group();
-			String imageUrl = imageTag.substring(imageTag.indexOf("(") + 1, imageTag.length() - 1);
+			String imageUrl = imageTag.substring(imageTag.indexOf(IMAGE_TAG_SPLITOR) + 1, imageTag.length() - 1);
 			allMatches.add(imageUrl);
 		}
-
-		return allMatches;
 	}
 }
